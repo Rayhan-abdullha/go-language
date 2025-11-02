76d47dc1ebe9d855af34472da443814fe68d0a0d
Rayhan-abdullha <captainrayhan215@gmail.com>
2025-11-02 13:46:56 +0600

    practice go

---
+++ lesson-9/main.go
@@ -0,0 +1,54 @@
+package main
+
+func main() {
+	// arr := [5]int{5, 2, 9, 44, 8}
+	// for i := range len(arr) {
+	// 	// if i > 5 {
+	// 	// 	continue
+	// 	// }
+	// 	println(arr[i])
+	// }
+
+	// 2D Array
+	// nums := [2][2]int{{2, 3}, {3, 5}}
+	// fmt.Println(nums)
+
+	// for i := range 10 {
+	// 	println(i)
+	// }
+
+	// we can create var inside if condition
+	/*
+		if age := 10; age > 18 {
+			println("You can watch Zero movie")
+		} else {
+			println("Warning!")
+		}
+	*/
+
+	/*
+		switch time.Now().Weekday() {
+		case time.Saturday, time.Friday:
+			println("Office is closed, enjoy weekend")
+		default:
+			println("Working Day")
+		}
+	*/
+
+	/*
+		for i := range 5 {
+			switch i {
+			case 1:
+				fmt.Println(i, " is not prime number")
+			case 2:
+				fmt.Println(i, " is prime number")
+			case 3:
+				fmt.Println(i, " is not prime number")
+			case 4:
+				fmt.Println(i, " is prime number")
+			case 5:
+				fmt.Println(i, " is not prime number")
+				}
+			}
+	*/
+}
